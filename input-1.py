0b8cb4ddb883343a8e2f9406390e17b9a389f7ef
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-30 10:15:49 +0530

    Taking input from user

---
+++ input-1.py
@@ -0,0 +1,11 @@
+# input inPython
+# input() statement is used to accept values(using keyboard) from the user.
+
+# .string input
+name = input("name : ") # print: name :
+
+# . int input
+age = int(input("age : "))  # print: age:
+
+# . float input
+price = float(input("price : "))     # print: price :
