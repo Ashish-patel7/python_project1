00678da9beb0dbf2cc8484aba6087b6d5f17dca3
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-30 17:59:51 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,26 @@
+# State true or false
+"""
+ 1. /* is a symbol used in Python for single line comment.(False)
+ 2. 2ndNAme is an invalid identifier in Python. (True)
+ 3. ** is a valid arithmetic operator in python. (True)
+ 4. in is a logical operator in Python. (False)
+ 5. variable declaration is implicit in python. (True)
+
+"""
+
+# a ** b == a ^ b
+# logical operator is and, or, not
+
+"""
+ 6. Consider the given expression: not True and False or True
+ which of thhe following will be correct output if the given expression is evaluated?
+ (a) True
+ (b) False
+ (c) NONE
+ (d) NULL
+ Answer: (a) True
+"""
+#Explaination:
+# not True means False
+# False and False means False
+# False or True means True
