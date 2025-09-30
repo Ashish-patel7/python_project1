531682dd8846074b507fb6844de477ed8391813b
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-30 18:33:46 +0530

    If, elif, else conditional statement

---
+++ condition1.py
@@ -0,0 +1,11 @@
+# Traffic Lights Code
+
+light = input("light: ")
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("slow down")
+elif(light == "green"):
+    print("go")
+else:
+    print("invalid light color")
