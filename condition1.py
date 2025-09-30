f8f53ec11da8aa8b24a2e25536b01fb5f0493ed9
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-30 18:53:26 +0530

    if, elif, else condition problem solve

--- condition1.py
+++ condition1.py
@@ -9,3 +9,15 @@ elif(light == "green"):
     print("go")
 else:
     print("invalid light color")
+
+# Grades of students
+
+marks = int(input("marks : "))
+if(marks >= 90):
+    print("A")
+elif(marks >= 80 and marks < 90):
+    print("B")
+elif(marks >= 70 and marks < 80):
+    print("C")
+else:
+    print("D")
