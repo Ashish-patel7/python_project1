e09036360ac5121e0eceacf386cc5f86bdf693fa
Ashish Kumar Patel <ashishkumar7773333@gmail.com>
2025-09-30 18:23:54 +0530

    Conditional Statements

---
+++ conditional.py
@@ -0,0 +1,16 @@
+# Conditional Statements
+
+"""
+if-elif-else (SYNTAX)
+
+if(condition):
+    Statement1
+elif(condition):
+    Statement2
+else:
+    StatementN
+
+"""
+# python is Indentation language (means proper spacing is necessary)
+# if-elif-else is used to execute a block of code among multiple blocks based on conditions
+
